fc5779dc8f1f2fca144d30bd50042855dabac5ed
Rob Baker <rrbaker@gmail.com>
2011-09-30 07:08:41 +0800

    added revela theme start (static files, need integration)

---
+++ themes/revela/readme.txt
@@ -0,0 +1,7 @@
+Theme Name: Revela
+Description: Default theme for the Revela website
+Demo: http://revela.com/
+Version: 1.0
+Author: Rob Baker
+Author Email: rrbaker@gmail.com
+Checkins: 1
---
+++ themes/revela/views/main.php
@@ -0,0 +1,280 @@
+<!doctype html>
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
+<!-- Consider adding an manifest.appcache: h5bp.com/d/Offline -->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Revela</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<link rel="stylesheet" href="/themes/revela/css/style.css">
+	<script src="/themes/revela/js/libs/modernizr-2.0.6.min.js"></script>
+</head>
+<body class="home">
+	<!-- container -->
+	<div id="container">
+		<!-- header -->
+		<header id="header">
+			<div class="header-holder clearfix">
+				<h1 class="logo ir"><a href="#" class="png_bg">Revela</a></h1>
+				<strong class="logo2 ir"><a href="#">Revela.org.br. Powed by the Ushahidi Platform</a></strong>
+				<nav>
+					<!-- top-list -->
+					<ul class="top-list">
+						<li><a href="#">. O que é?</a></li>
+						<li><a href="#">. Quem somos?</a></li>
+						<li><a href="#">. Links úteis</a></li>
+					</ul>
+				</nav>
+				<!-- top-info -->
+				<div class="top-info">
+					<span>Apoios e Patrocínios:</span>
+				</div>
+			</div>
+		</header>
+		<!-- main -->
+		<div id="main" role="main">
+			<!-- content -->
+			<article id="content">
+				<!-- map-block -->
+				<section class="map-block png_bg">
+					<div class="holder png_bg">
+						<div class="frame">
+							<div class="holder-frame">
+								<!-- nav -->
+								<nav class="nav">
+									<ul>
+										<li><a href="#"><strong>|<span>Como Revelar</span>|</strong></a></li>
+										<li><a href="#"><strong>|<span>Envolva-se</span>|</strong></a></li>
+										<li><a href="#"><strong>|<span>Envie Para um Amigo</span>|</strong></a></li>
+										<li><a href="#"><strong>|<span>Contatos </span>|</strong></a></li>
+										<li><a href="#"><strong>|<span>Participações</span>|</strong></a></li>
+									</ul>
+								</nav>
+								<!-- map -->
+								<div class="map">
+									<img src="images/map.jpg" alt="map" width="635" height="627" />
+									<span class="flag png_bg">13</span>
+									<!-- box -->
+									<div class="box png_bg">
+										<div class="box-holder png_bg">
+											<div class="box-frame">
+												<!-- box-content -->
+												<div class="box-content">
+													<h2>Amazonas: Quadro das atividades.</h2>
+													<!-- item-list -->
+													<div class="item-list">
+														<ul>
+															<li>
+																<a href="#">
+																	<span class="sector">04<span class="mask png_bg"></span></span>
+																	<strong>Queimada</strong>
+																</a>
+															</li>
+															<li>
+																<a href="#">
+																	<span class="sector brown-sector">03<span class="mask png_bg"></span></span>
+																	<strong>Desmatamento</strong>
+																</a>
+															</li>
+															<li>
+																<a href="#">
+																	<span class="sector orange-sector">01<span class="mask png_bg"></span></span>
+																	<strong>Enchente</strong>
+																</a>
+															</li>
+															<li>
+																<a href="#">
+																	<span class="sector olive-sector">02<span class="mask png_bg"></span></span>
+																	<strong>Proteção de Morro</strong>
+																</a>
+															</li>
+															<li>
+																<a href="#">
+																	<span class="sector green-sector">01<span class="mask png_bg"></span></span>
+																	<strong>Coleta Seletiva</strong>
+																</a>
+															</li>
+														</ul>
+													</div>
+													<!-- nav -->
+													<nav class="nav">
+														<ul>
+															<li><a href="#"><strong>|<span>Detalhar</span>|</strong></a></li>
+															<li><a href="#"><strong>|<span>Comentários</span>|</strong></a></li>
+															<li><a href="#"><strong>|<span>Compartilhar</span>|</strong></a></li>
+														</ul>
+													</nav>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+				<!-- content-block -->
+				<article class="content-block png_bg">
+					<div class="holder png_bg">
+						<div class="frame">
+							<div class="holder-frame">
+								<!-- video-block -->
+								<div class="video-block">
+									<div class="video-holder">
+										<img src="images/img1.jpg" alt="image description" width="599" height="338" />
+										<h2>Assista os vídeos da campanha sobre o novo do Código Florestal</h2>
+									</div>
+									<!-- add-nav -->
+									<nav class="add-nav">
+										<ul>
+											<li class="active"><a href="#" class="png_bg"><strong class="png_bg">|<span>Comentários</span>|</strong></a></li>
+											<li><a href="#" class="png_bg"><strong class="png_bg">|<span>Compartilhar</span>|</strong></a></li>
+										</ul>
+									</nav>
+								</div>
+								<!-- comment-block -->
+								<div class="comment-block">
+									<ul>
+										<li>
+											<p>Como negociar com ignorantes cheio de grana???</p>
+											<footer class="posted"><a href="#">osenhortodopoderoso</a> <time datetime="2011-09-21T16:00" pubdate="">1 hour ago</time></footer>
+										</li>
+										<li>
+											<p>A mesma mentalidade de 500 anos atrás, sugar os recursos naturais até o fim e depois vender soluções para os problemas que foram criados.</p>
+											<p>Quem são os grandes fora-da-lei?? A agricultura familiar ou a Agricultura patronal???</p>
+											<footer class="posted"><a href="#">aldenliuti</a> <time datetime="2011-09-21T5:00" pubdate="">12 hours ago</time></footer>
+										</li>
+										<li>
+											<p>EXISTEM MUITOS ARTIGOS TECNICO/CIENTIFICOS ATUAIS, LEIAM! Parecer do Ministério público, analistas ambientais, pesquisadores das instituições de ensino, trabalhos publicados...</p>
+											<footer class="posted"><a href="#">Albebasiiii</a> <time datetime="2011-02-21T17:00" pubdate="">1 weeks ago</time></footer>
+										</li>
+										<li>
+											<p>Antes de se preocupar com o mundo,preocupe-se com si mesmo.Se a questão é a fome, parem de exportar e negociem com a população faminta do Brasil.</p>
+											<footer class="posted"><a href="#">andersonsilvabass</a> <time datetime="2011-02-21T17:00" pubdate="">1 weeks ago</time></footer>
+										</li>
+									</ul>
+									<strong class="info">ATENÇÃO! GOVERNO FEDERAL!!!</strong>
+								</div>
+								<footer>
+									<h3>Comentário em destaque..........................</h3>
+								</footer>
+							</div>
+						</div>
+					</div>
+				</article>
+			</article>
+			<aside id="sidebar">
+				<h2 class="text-destaque ir png_bg">destaque</h2>
+				<ul class="sub-nav">
+					<li><a href="#">Terça-feira 20/09<br/>inicia a votação do novo<br/>código florestal.</a></li>
+					<li class="active"><a href="#">Participe e dê sua opinião!</a></li>
+				</ul>
+				<div class="sub-box png_bg">
+					<div class="holder png_bg">
+						<div class="frame">
+							<div class="holder-frame">
+								<div class="sub-list">
+									<h2 class="text-noticas ir png_bg">Noticias</h2>
+									<div class="list-block">
+										<div class="list-holder">
+											<ul>
+												<li>
+													<em class="date"><span class="year">20/12/11</span> 12:45</em>
+													<p>A multinacional de vendas porta a porta de cosméticos Mary Kay... <a href="#" class="more">Leia +</a></p>
+												</li>
+												<li>
+													<em class="date"><span class="year">20/12/11</span> 16:51</em>
+													<p>Volta a veicular campa nha no Brasil. Criada pela Sociedade da Comunicação... <a href="#" class="more">Leia +</a></p>
+												</li>
+												<li>
+													<em class="date"><span class="year">20/12/11</span> 18:40</em>
+													<p>Timewise Microdermo abrasão. Integrando a campanha que a Mary Kay realizou para Latina... <a href="#" class="more">Leia +</a></p>
+												</li>
+												<li>
+													<em class="date"><span class="year">20/12/11</span> 20:21</em>
+													<p>Com adaptações para o mercado brasileiro, os anúncios de página dupla serão veiculados... <a href="#" class="more">Leia +</a></p>
+												</li>
+												<li>
+													<em class="date"><span class="year">20/12/11</span> 20:21</em>
+													<p>O kit traz um conjunto de cosméticos para reduzir as linhas finas de revitalizar a epiderme... <a href="#" class="more">Leia +</a></p>
+												</li>
+											</ul>
+										</div>
+									</div>
+								</div>
+								<div class="twitter-listblock">
+									<div class="heading">
+										<a href="#" class="twitter-icon ir">twitter</a>
+										<h2 class="text-twitter ir png_bg">Posts Tweeter</h2>
+									</div>
+									<div class="list-block">
+										<div class="list-holder">
+											<ul class="twitter-list">
+												<li>
+													<img class="alignleft" src="images/img2.gif" alt="image description" width="51" height="51" />
+													<p>that's the # RT <a href="#">@majiwater: Just set up</a> <a href="#">http://t.co/2p5Z4Jt</a> calling for volunteers/moderators familiar with <a href="#">#ushahidi #ZanzibarBoatAccident</a></p>
+												</li>
+												<li>
+													<img class="alignleft" src="images/img2.gif" alt="image description" width="51" height="51" />
+													<p>Just set up <a href="#">http://t.co/EXesezO</a> calling for volunteers/moderators familiar with <a href="#">#ushahidi #ZanzibarBoatDisaster via @majiwate</a></p>
+												</li>
+												<li>
+													<img class="alignleft" src="images/img2.gif" alt="image description" width="51" height="51" />
+													<p>Young Africans put technology to new uses: <a href="#">http://t.co/R0dRVdv</a> <a href="#">via @pablarradar #mobile #telephony via @LisaCespedes</a></p>
+												</li>
+											</ul>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</aside>
+		</div>
+		<!-- footer -->
+		<footer id="footer">
+			<div class="footer-holder png_bg">
+				<div class="footer-frame png_bg">
+					<!-- footer-block -->
+					<div class="footer-block">
+						<ul>
+							<li><a href="#">Texto Legal</a></li>
+							<li><a href="#">Política de Privacidade</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</div> <!--! end of #container -->
+
+	<!-- JavaScript at the bottom for fast page loading -->
+	<!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
+	
+	<!--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="/themes/revela/js/libs/jquery-1.6.2.min.js"><\/script>')</script>-->
+	
+	<!-- scripts concatenated and minified via ant build script-->
+	<script defer src="/themes/revela/js/plugins.js"></script>
+	<script defer src="/themes/revela/js/script.js"></script>
+	<!-- end scripts-->
+	<!-- Change UA-XXXXX-X to be your site's ID -->
+	<!--<script>
+		window._gaq = [['_setAccount','UAXXXXXXXX1'],['_trackPageview'],['_trackPageLoadTime']];
+		Modernizr.load({
+		load: ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js'
+		});
+	</script>-->
+	<!-- Prompt IE 6 users to install Chrome Frame. Remove this if you want to support IE 6.
+		chromium.org/developers/how-tos/chrome-frame-getting-started -->
+	<!--[if lt IE 7 ]>
+		<script src="/themes/revela/js/DD_belatedPNG_0.0.8a-min.js"></script>
+	<![endif]-->
+</body>
+</html>
